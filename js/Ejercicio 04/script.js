3b569aee2e75dfa3e9c1cdc84f1322b0ffb77ceb
jaume vidal <jaumevibu@gmail.com>
2022-12-05 19:10:31 +0100

    Added Ejercicio 04 to JS course

---
+++ js/Ejercicio 04/script.js
@@ -0,0 +1,12 @@
+const miNombre = 'Jaume';
+const misApellidos = 'Vidal Buenafuente';
+const estudiante = miNombre.concat(' ', misApellidos);
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const numeroDeLetrasDeEstudiante = estudiante.length;
+const primeraLetraDeMiNombre = miNombre[0];
+const ultimaLetraDeMiApellido = misApellidos[misApellidos.length - 1];
+const estudianteSinEspacios = estudiante.replace(/ /g, '');
+const isMiNombreInsideEstudiante = estudiante.includes(miNombre);
+
+
