6ea744dfc2a3b254cd8af2ce558bb7119d1174ea
jaume vidal <jaumevibu@gmail.com>
2022-12-05 19:39:32 +0100

    Added Ejercicio 05 to JS course

---
+++ js/Ejercicio 05/script.js
@@ -0,0 +1,6 @@
+const miAlturaCm = 170; //aprox, hace muuucho tiempo que no me mido...
+const miAlturaM = 1.7;
+const miPesoKg = 68.5;
+const miAlturaMRedondeadaArriba = Math.round(miAlturaM);
+const miPesoKgRedondeadoAbajo = Math.floor(miPesoKg);
+const esPosibleSumar1AlMaximo = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
