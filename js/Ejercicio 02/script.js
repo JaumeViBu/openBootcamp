20e87ed73cb1f0db6373200e4cbd407e5155c1f1
jaume vidal <jaumevibu@gmail.com>
2022-12-05 14:47:13 +0100

    Added ejercicio 02  to js course

---
+++ js/Ejercicio 02/script.js
@@ -0,0 +1,24 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+// - Tu nombre (string)
+
+// - Tu edad (number)
+
+// - ¿Eres desarrollador? (boolean)
+
+// - Tu fecha de nacimiento (Date)
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const lista = [
+  'Jaume Vidal Buenafuente',
+  '33',
+  true,
+  new Date('01/01/1989'),
+  {
+    titulo: "Tokio Blues",
+    autor: "Haruki Murakami",
+    fecha: new Date('04/09/1987'),
+    url: 'https://es.wikipedia.org/wiki/Tokio_blues_(Norwegian_Wood)',
+  }
+];
