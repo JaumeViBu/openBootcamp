9d5aab1a5aa5505ca3848f553172e2109994abb9
jaume vidal <jaumevibu@gmail.com>
2022-12-17 08:43:54 +0100

    JS Ejercicio18 - sessionStorage done

--- js/Ejercicio18/index.js
+++ js/Ejercicio18/index.js
@@ -1,11 +1,3 @@
-// Crea un nuevo proyecto de Node
-// - Crea un fichero index.html (utiliza el comando "!")
-// - Crea un fichero index.js
-// - Integra el fichero index.js en el html
-// - En el fichero index.js:
-// - Crea una variable con tu nombre
-// - Crea una variable con tu apellido
-// - Crea un objeto con tu nombre y tu apellido
 // - Almacena el objeto anterior en la SessionStorage
 // - Almacena el objeto anterior en la LocalStorage
 // - Crea una cookie que caduque en 2 minutos con los datos del objeto anterior
@@ -14,3 +6,11 @@
 // - Observa cómo la SessionStorage está vacía
 // - Observa cómo la LocalStorage sigue manteniendo el objeto que has almacenado antes de cerrar el navegador
 // - Observa cómo la cookie sigue manteniendo el objeto que has almacenado antes, aunque ya está caducado
+
+const miNombre = "Jaume";
+const miApellido = "Vidal";
+
+const miPersona = {
+  nombre: miNombre,
+  apellido: miApellido
+};
