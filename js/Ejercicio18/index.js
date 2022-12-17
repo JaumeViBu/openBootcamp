6bf724147f37502583f2602d022af9b18c7b8cfd
jaume vidal <jaumevibu@gmail.com>
2022-12-17 08:55:17 +0100

    Commented storage settings and examined the behaviour in the browser

--- js/Ejercicio18/index.js
+++ js/Ejercicio18/index.js
@@ -1,9 +1,3 @@
-// - Observa en Google Chrome cómo se almacenan los datos en la SessionStorage, LocalStorage y las cookies
-// - Cierra el navegador, comenta las líneas que almacenan SessionStorage, LocalStorage y CookieStorage y vuelve a abrirlo
-// - Observa cómo la SessionStorage está vacía
-// - Observa cómo la LocalStorage sigue manteniendo el objeto que has almacenado antes de cerrar el navegador
-// - Observa cómo la cookie sigue manteniendo el objeto que has almacenado antes, aunque ya está caducado
-
 const miNombre = "Jaume";
 const miApellido = "Vidal";
 
@@ -14,10 +8,10 @@ const miPersona = {
 
 const miPersonaStringified = JSON.stringify(miPersona);
 
-sessionStorage.setItem("miPersona", miPersonaStringified);
-localStorage.setItem("miPersona", miPersonaStringified);
+// sessionStorage.setItem("miPersona", miPersonaStringified);
+// localStorage.setItem("miPersona", miPersonaStringified);
 
-const expireTime = new Date();
-expireTime.setMinutes(expireTime.getMinutes() + 2);
+// const expireTime = new Date();
+// expireTime.setMinutes(expireTime.getMinutes() + 2);
 
-document.cookie = `miPersona=${miPersonaStringified}; expires=${expireTime.toUTCString()}`;
+// document.cookie = `miPersona=${miPersonaStringified}; expires=${expireTime.toUTCString()}`;
